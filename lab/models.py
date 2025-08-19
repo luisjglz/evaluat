46eadad1073a9f7cd477d6ebca64ae0f44329df1
DiegoMurielG <yinga100@gmail.com>
2025-08-18 23:48:50 -0600

    Added Laboratorio table

--- lab/models.py
+++ lab/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class Laboratorio(models.Model):
     nombre = models.CharField(max_length=200)
-    clave = models.TextField(blank=True)
+    clave = models.CharField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    contacto = models.CharField(blank=True)
+    correo = models.EmailField(blank=True)
 
     def __str__(self):
         return self.name
