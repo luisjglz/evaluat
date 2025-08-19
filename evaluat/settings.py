981c08919e9782a576d100f93ffe004326e66128
liusglz <a00924922@tec.mx>
2025-08-18 22:37:06 -0600

    EvaluaT

--- evaluat/settings.py
+++ evaluat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+2aeq1%t00sle71f+mqy@(5)h_#5hs(%n8squ%^cu)$qxoy$_0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evaluat.pythonanywhere.com']
 
 
 # Application definition
