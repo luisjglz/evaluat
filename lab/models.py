e28ae9e70d81006e2f19fc70f379d8944a6e7f22
liusglz <a00924922@tec.mx>
2025-08-20 12:46:22 -0600

    Combina las migraciones en 1 sola

--- lab/models.py
+++ lab/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Laboratorio(models.Model):
-    nombre = models.CharField(max_length=200)
-    clave = models.CharField(blank=True)
+    nombre = models.CharField(max_length=255)
+    clave = models.CharField(max_length=255)
+    contacto = models.CharField(max_length=255)
+    correo = models.EmailField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    contacto = models.CharField(blank=True)
-    correo = models.EmailField(blank=True)
 
     def __str__(self):
         return self.nombre
