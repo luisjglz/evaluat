0071dc38bdad46cc57e46c141c6da6421b210310
liusglz <a00924922@tec.mx>
2025-08-20 23:58:13 -0600

    Añadiendo templates y estilos

--- lab/admin.py
+++ lab/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import Laboratorio
+from django.apps import apps
 
-admin.site.register(Laboratorio)
+# Get all models from the current app
+models = apps.get_models()
+
+# Models you want to hide from the admin
+excluded_models = ['LogEntry', 'Permission', 'Groups', 'Session', 'ContentType']  # <- use class names here
+
+for model in models:
+    if model.__name__ in excluded_models:
+        continue  # skip these models
+    else:
+        print(model.__name__)
+        try:
+            admin.site.register(model)
+        except admin.sites.AlreadyRegistered:
+            pass
--- lab/models.py
+++ lab/models.py
@@ -9,3 +9,49 @@ class Laboratorio(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Programa(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nombre
+    
+class MetodoAnalitico(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Instrumento(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Reactivo(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+
+class UnidadDeMedida(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Prueba(models.Model):
+    nombre = models.CharField(max_length=255)
+    metodo_analitico = models.ForeignKey(MetodoAnalitico, on_delete=models.CASCADE, related_name='pruebas')
+    instrumento = models.ForeignKey(Instrumento, on_delete=models.CASCADE, related_name='pruebas')
+    reactivo = models.ForeignKey(Reactivo, on_delete=models.CASCADE, related_name='pruebas')
+    unidad_de_medida = models.ForeignKey(UnidadDeMedida, on_delete=models.CASCADE, related_name='pruebas')
+    programa = models.ForeignKey("Programa", on_delete=models.CASCADE, related_name='pruebas')
+    
+    def __str__(self):
+        return self.nombre
